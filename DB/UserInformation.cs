182a085f66384675aa7c5eeaaeb567b16458d77f
KnightTheCoder <kocsis_dominik@freemail.hu>
2022-02-21 23:16:55 +0100

    Added documentation for user information class

--- DB/UserInformation.cs
+++ DB/UserInformation.cs
@@ -1,15 +1,30 @@
 namespace Knight.MysqlTest2.DB
 {
+    /// <summary>
+    /// Represents the user's information
+    /// </summary>
     public class UserInformation
     {
         private string? firstName;
         private string? lastName;
         private string? gender;
 
+        /// <summary>
+        /// The user's first name
+        /// </summary>
         public string? FirstName { get => this.firstName; }
+        /// <summary>
+        /// The user's last name
+        /// </summary>
         public string? LastName { get => this.lastName; }
+        /// <summary>
+        /// The user's gender
+        /// </summary>
         public string? Gender { get => this.gender; }
 
+        /// <summary>
+        /// Initializes an empty <see cref="UserInformation"></see> object
+        /// </summary>
         public UserInformation()
         {
             this.firstName = null;
@@ -17,6 +32,12 @@ namespace Knight.MysqlTest2.DB
             this.gender = null;
         }
 
+        /// <summary>
+        /// Initializes a <see cref="UserInformation"></see> object with specified information
+        /// </summary>
+        /// <param name="firstName">Firstname of the user</param>
+        /// <param name="lastName">Lastname of the user</param>
+        /// <param name="gender">Gender of the user</param>
         public UserInformation(string? firstName, string? lastName, string? gender)
         {
             this.firstName = firstName;
