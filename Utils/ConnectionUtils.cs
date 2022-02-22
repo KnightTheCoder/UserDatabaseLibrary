833ce099e98b9a0dfaaecbae682db92b5472c78b
KnightTheCoder <kocsis_dominik@freemail.hu>
2022-02-22 07:03:01 +0100

    Added documentation for connection utils

--- Utils/ConnectionUtils.cs
+++ Utils/ConnectionUtils.cs
@@ -2,8 +2,16 @@ using System.Text.Json;
 
 namespace Knight.MysqlTest2.Utils
 {
+    /// <summary>
+    /// A static class containing connection utilities
+    /// </summary>
     public static class ConnectionUtils
     {
+        /// <summary>
+        /// Gets the connection information from a json file
+        /// </summary>
+        /// <param name="filePath">Location of the json file</param>
+        /// <returns>Connection info as a <see cref="DatabaseConectionInfo"></see> object</returns>
         public static DatabaseConectionInfo? GetConnectionInfo(string filePath)
         {
             FileStream infoFile = File.Open(filePath, FileMode.Open);
