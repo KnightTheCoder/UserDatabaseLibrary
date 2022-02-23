4a237555cd439e0c16eecfee75abca221b5c0250
KnightTheCoder <kocsis_dominik@freemail.hu>
2022-02-23 16:19:55 +0100

    Added an exception for handling registration

---
+++ Exceptions/RegistrationFailedException.cs
@@ -0,0 +1,36 @@
+namespace Knight.MysqlTest2.Exceptions
+{
+    /// <summary>
+    /// Occurs when a registration fails
+    /// </summary>
+    public class RegistrationFailedException: UserDatabaseException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistrationFailedException"></see> class with a default error message.
+        /// </summary>
+        public RegistrationFailedException()
+        : base("Registration failed")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistrationFailedException"></see> class with a specified error message.
+        /// </summary>
+        /// <param name="message"></param>
+        public RegistrationFailedException(string message)
+        : base (message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistrationFailedException"></see> class with specified error message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public RegistrationFailedException(string message, Exception innerException) 
+        : base(message, innerException)
+        {
+        }
+
+    }
+}
